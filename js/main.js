60436efd19b472fcb3db31dde8cfb1b252f220fc
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-02-12 16:08:48 -0800

    Assessment complete

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", loadDOM)
+
+
+// DOM elements
+function loadDOM() {
+var addButton = document.getElementById(addButton);
+var subtractButton = document.getElementById(subtractButton);
+var input = document.getElementById(input);
+var num = document.getElementById(num);
+var form1 = document.getElementById(form1);
+};
+
+addButton.addEventListener("click", function(e){
+    e.preventDefault();
+    var number = parseInt(num.textContent);
+    var newNumber = parseInt(number) + parseInt(input.value);
+    num.textContent = newNumber;
+    console.log(newNumber);
+});
+
+subtractButton.addEventListener("click", function(e){
+    e.preventDefault();
+    var number = parseInt(num.textContent);
+    var newNumber = parseInt(number) - parseInt(input.value);
+    num.textContent = newNumber;
+    console.log(newNumber);
+
+});
